d7fa730130bab52be3f7a42e4730598fa66f80d8
kantorujbitdev <kantorujbitdev@gmail.com>
2025-09-14 21:30:47 +0700

    Update sidebar.php

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,3 +1,4 @@
+Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center"
